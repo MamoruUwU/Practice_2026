3885ccfd9642faf5dd4e91964dfce80bda86eb33
Oleksandr Unhurian <unhurian.oleksandr.y@chnu.edu.ua>
2026-02-10 20:51:55 +0200

    Lab2

---
+++ Lab2/src/main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <chrono>
+#include <mutex>
+#include <cmath>
+
+using namespace std;
+
+// Константи
+const int WIDTH = 100;
+const int HEIGHT = 30;
+const int SPEED = 1;
+const int DELAY_MS = 100;
+const int RADIUS = 10;
+
+// М'ютекс для синхронізації виводу
+mutex printMutex;
+
+/* =========================
+   МУРАХА-РОБОЧА
+   ========================= */
+class WorkerAnt {
+public:
+    string name;
+    int x, y;
+    int startX, startY;
+    bool goingToCorner;
+
+    WorkerAnt(string name, int x, int y)
+        : name(name), x(x), y(y),
+          startX(x), startY(y),
+          goingToCorner(true) {}
+
+    void move() {
+        while (true) {
+            {
+                lock_guard<mutex> lock(printMutex);
+                cout << name << " (робоча) x=" << x << " y=" << y << endl;
+            }
+
+            if (goingToCorner) {
+                if (x > 0) x -= SPEED;
+                if (y > 0) y -= SPEED;
+
+                if (x == 0 && y == 0)
+                    goingToCorner = false;
+            } else {
+                if (x < startX) x += SPEED;
+                if (y < startY) y += SPEED;
+
+                if (x == startX && y == startY)
+                    goingToCorner = true;
+            }
+
+            this_thread::sleep_for(chrono::milliseconds(DELAY_MS));
+        }
+    }
+};
+
+/* =========================
+   МУРАХА-ВОЇН
+   ========================= */
+class WarriorAnt {
+public:
+    string name;
+    double angle;
+    int centerX, centerY;
+    int R;
+
+    WarriorAnt(string name, int cx, int cy, int r)
+        : name(name), angle(0.0),
+          centerX(cx), centerY(cy), R(r) {}
+
+    void move() {
+        while (true) {
+            int x = centerX + static_cast<int>(R * cos(angle));
+            int y = centerY + static_cast<int>(R * sin(angle));
+
+            {
+                lock_guard<mutex> lock(printMutex);
+                cout << name << " (воїн) x=" << x << " y=" << y << endl;
+            }
+
+            angle += 0.1 * SPEED;
+
+            this_thread::sleep_for(chrono::milliseconds(DELAY_MS));
+        }
+    }
+};
+
+/* =========================
+              main
+   ========================= */
+int main() {
+    vector<WorkerAnt> workers = {
+        WorkerAnt("Worker_1", 20, 10),
+        WorkerAnt("Worker_2", 40, 20)
+    };
+
+    vector<WarriorAnt> warriors = {
+        WarriorAnt("Warrior_1", 50, 15, RADIUS),
+        WarriorAnt("Warrior_2", 70, 10, RADIUS)
+    };
+
+    vector<thread> threads;
+
+    threads.emplace_back(&WorkerAnt::move, &workers[0]);
+    threads.emplace_back(&WorkerAnt::move, &workers[1]);
+
+    threads.emplace_back(&WarriorAnt::move, &warriors[0]);
+    threads.emplace_back(&WarriorAnt::move, &warriors[1]);
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    return 0;
+}
